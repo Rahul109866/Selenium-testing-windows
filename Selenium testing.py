66f343c23c83f41da0f624c32f662744bd7040a1
Rahul Rajendran <126910185+Rahul109866@users.noreply.github.com>
2023-04-26 17:56:28 +0530

    Update Selenium testing.py

--- Selenium testing.py
+++ Selenium testing.py
@@ -1,3 +1,4 @@
+# requisites
 from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
@@ -5,27 +6,30 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
+# chrome driver updater and path setting
 chromedriver_autoinstaller.install()
 
-driver = webdriver.Chrome()
+# open Chrome and Navigate to LinkedIn Website
+driver = webdriver.Chrome()  # open Chrome browser
 driver.get("https://in.linkedin.com/")
-driver.maximize_window()
+driver.maximize_window()  # maximize the Chrome window
 driver.implicitly_wait(4)  # give the browser time to load the page fully
 
+# assigning the target fields to variables
 username_field = driver.find_element(by=By.XPATH, value="//input[@id='session_key']")
 password_field = driver.find_element(by=By.XPATH, value="//input[@id='session_password']")
+# navigate and enter username
 username_field.click()
-
 username_field.send_keys("rahulrajendran1098@gmail.com")
 
+# navigate and enter password
 password_field.click()
+password_field.send_keys("password")
 
-password_field.send_keys("faker.lol098")
-
+# navigate to and click submit
 submit_button = driver.find_element(by=By.XPATH,
                                     value="//*[@id='main-content']/section[1]/div/div/form[1]/div[2]/button")
-
 submit_button.click()
 
-time.sleep(30)
-driver.quit()
+time.sleep(30)  # time the browser is opened. Have to mention explicitly due to automatic closing
+driver.quit()  # close chrome
